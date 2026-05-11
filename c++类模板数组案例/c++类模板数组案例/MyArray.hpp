ae1baef1c8d778200d432604a54565d6db473f6f
yunxia223 <1980805871@qq.com>
2026-05-11 13:35:12 +0800

    C++类模板数组案例

---
+++ c++类模板数组案例/c++类模板数组案例/MyArray.hpp
@@ -0,0 +1,93 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+
+template<class T>
+class MyArray {
+public:
+	//Constructor with parameter
+	MyArray(int capacity) {
+		//cout << "MyArray constructor called" << endl;
+		this->m_Capacity = capacity;
+		this->m_Size = 0;
+		this->pAddress = new T[this->m_Capacity];
+	}
+	
+	//Copy constructor
+	MyArray(const MyArray& arr) {
+		//cout << "MyArray copy constructor called" << endl;
+		this->m_Capacity = arr.m_Capacity;
+		this->m_Size = arr.m_Size;
+		this->pAddress = new T[arr.m_Capacity];
+
+		for (int i = 0; i < this->m_Size; i++) {
+			this->pAddress[i] = arr.pAddress[i];
+		}
+	}
+
+	//Assignment operator to prevent shallow copy
+	MyArray& operator=(const MyArray& arr) {
+		//cout << "MyArray operator= called" << endl;
+		if (this->pAddress != nullptr) {
+			delete[] this->pAddress;
+			this->pAddress = nullptr;
+			this->m_Capacity = 0;
+			this->m_Size = 0;
+		}
+
+		this->m_Capacity = arr.m_Capacity;
+		this->m_Size = arr.m_Size;
+		this->pAddress = new T[arr.m_Capacity];
+		for (int i = 0; i < this->m_Size; i++) {
+			this->pAddress[i] = arr.pAddress[i];
+		}
+		return *this;
+	}
+
+	//尾插法
+	void Push_Back(const T& val) {
+		if (this->m_Capacity == this->m_Size) {
+			return;
+		}
+		this->pAddress[this->m_Size] = val;
+		this->m_Size++;
+	}
+
+	//尾删法
+	void Pop_Back() {
+		if (this->m_Size == 0) {
+			return;
+		}
+		this->m_Size--;
+	}
+
+	//通过下标访问数据
+	T& operator[](int index) {
+		return this->pAddress[index];
+	}
+
+	//返回数组容量
+	int getCapacity() {
+		return this->m_Capacity;
+	}
+
+	int getSize() {
+		return this->m_Size;
+	}
+
+	//Destructor
+	~MyArray() {
+		//cout << "MyArray destructor called" << endl;
+		if (this->pAddress != nullptr) {
+			delete[] this->pAddress;
+			this->pAddress = nullptr;
+		}
+	}
+
+private:
+	T* pAddress;
+	
+	int m_Capacity;
+
+	int m_Size;
+};
---
+++ c++类模板数组案例/c++类模板数组案例/源.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <string>
+#include "MyArray.hpp"
+using namespace std;
+
+class Person {
+public:
+	Person() {};
+	Person(string name, int age) {
+		this->m_Name = name;
+		this->m_Age = age;
+	}
+
+	string m_Name;
+	int m_Age;
+};
+
+void printIntArray(MyArray <int>& arr) {
+	for (int i = 0; i < arr.getSize(); i++) {
+		cout << arr[i]<<endl;
+	}
+}
+
+void printPersonArray(MyArray<Person>& arr) {
+	for (int i = 0; i < arr.getSize(); i++) {
+		cout << "姓名：" << arr[i].m_Name << "年龄：" << arr[i].m_Age << endl;
+	}
+}
+
+void test01() {
+	MyArray<int>arr1(5);
+
+	/*MyArray<int>arr2(arr1);
+
+	MyArray<int>arr3(100);
+	arr3 = arr1;*/
+	for (int i = 0; i < 5; i++) {
+		arr1.Push_Back(i);
+	}
+	cout << "arr1的打印输出为:" << endl;
+	printIntArray(arr1);
+
+	cout << "arr1的容量为：" << arr1.getCapacity() << endl;
+	cout << "arr1的大小为：" << arr1.getSize() << endl;
+
+	MyArray<int>arr2(arr1);
+	arr2.Pop_Back();
+	printIntArray(arr2);
+}
+
+
+
+void test02() {
+	MyArray<Person>arr(10);
+	Person p1("孙悟空", 999);
+	Person p2("韩信", 30);
+	Person p3("妲己", 20);
+	Person p4("赵云", 25);
+	Person p5("安琪拉", 18);
+
+	arr.Push_Back(p1);
+	arr.Push_Back(p2);
+	arr.Push_Back(p3);
+	arr.Push_Back(p4);
+	arr.Push_Back(p5);
+
+	printPersonArray(arr);
+	cout << "arr容量为：" << arr.getCapacity() << endl;
+	cout << "arr的大小为：" << arr.getSize() << endl;
+}
+
+int main() {
+	//test01();
+	test02();
+	system("pause");
+	return 0;
+}
